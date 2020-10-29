de2aaa0472963968b5d80853d64e57b5a4364d9c
Hariom Yadav <hoc.yadav@gmail.com>
2020-10-29 12:47:54 +0530

    kafka producer with keys and callback

--- src/main/java/com/example/demojava/kafka/KafkaProducer02Keys.java
+++ src/main/java/com/example/demojava/kafka/KafkaProducer02Keys.java
@@ -12,17 +12,21 @@ import org.apache.kafka.common.serialization.StringSerializer;
  */
 public class KafkaProducer02Keys {
     public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        final Properties properties = getProperties();
+        final KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
 
         //add key before value, same key makes msg to send to same partition
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "key01","hello from java");
+        final ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "key01","hello from java");
         kafkaProducer.send(producerRecord);//this is async, run in background
         kafkaProducer.flush();//make async to flush all data
         kafkaProducer.close();//optional
+    }
+
+    private static Properties getProperties() {
+        final Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
 }
--- src/main/java/com/example/demojava/kafka/KafkaProducer02KeysWithCallback.java
+++ src/main/java/com/example/demojava/kafka/KafkaProducer02KeysWithCallback.java
@@ -17,17 +17,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class KafkaProducer02KeysWithCallback {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        final Properties properties = getProperties();
+        final KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
 
         //add key before value
         for (int i = 0; i < 10; i++) {
             final String key = "key_id_"+Integer.valueOf(i); // same key will always go to same partition , it will not change its partition
             System.out.println("key = " + key);
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", key, "hello from java " + Integer.valueOf(i));
+            final ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", key, "hello from java " + Integer.valueOf(i));
             kafkaProducer.send(producerRecord, new Callback() {
                 @Override
                 public void onCompletion(RecordMetadata recordMetadata, Exception e) {
@@ -45,5 +42,13 @@ public class KafkaProducer02KeysWithCallback {
         }
         kafkaProducer.flush();//make async to flush all data
         kafkaProducer.close();//optional
+    }
+
+    private static Properties getProperties() {
+        final Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
 }
